d142f7e0417412d8515c1ec8e8d82d350f8f21de
Roncepido23 <oncepidorobin34@gmail.com>
2025-10-20 06:44:00 -0700

    Finished Designing but might change later if not satisfied

--- LabExer/FrmRegistration.Designer.cs
+++ LabExer/FrmRegistration.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRegistration));
             this.lblRegistration = new System.Windows.Forms.Label();
             this.lblStudentNo = new System.Windows.Forms.Label();
             this.lblLastName = new System.Windows.Forms.Label();
@@ -55,11 +54,11 @@
             // 
             this.lblRegistration.AutoSize = true;
             this.lblRegistration.BackColor = System.Drawing.Color.Transparent;
-            this.lblRegistration.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistration.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRegistration.ForeColor = System.Drawing.Color.White;
-            this.lblRegistration.Location = new System.Drawing.Point(12, 25);
+            this.lblRegistration.Location = new System.Drawing.Point(11, 22);
             this.lblRegistration.Name = "lblRegistration";
-            this.lblRegistration.Size = new System.Drawing.Size(199, 37);
+            this.lblRegistration.Size = new System.Drawing.Size(209, 43);
             this.lblRegistration.TabIndex = 0;
             this.lblRegistration.Text = "Registration";
             // 
@@ -67,11 +66,11 @@
             // 
             this.lblStudentNo.AutoSize = true;
             this.lblStudentNo.BackColor = System.Drawing.Color.Transparent;
-            this.lblStudentNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentNo.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblStudentNo.ForeColor = System.Drawing.Color.White;
-            this.lblStudentNo.Location = new System.Drawing.Point(15, 86);
+            this.lblStudentNo.Location = new System.Drawing.Point(15, 95);
             this.lblStudentNo.Name = "lblStudentNo";
-            this.lblStudentNo.Size = new System.Drawing.Size(109, 24);
+            this.lblStudentNo.Size = new System.Drawing.Size(99, 23);
             this.lblStudentNo.TabIndex = 1;
             this.lblStudentNo.Text = "Student No.";
             // 
@@ -79,12 +78,12 @@
             // 
             this.lblLastName.AutoSize = true;
             this.lblLastName.BackColor = System.Drawing.Color.Transparent;
-            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLastName.ForeColor = System.Drawing.Color.White;
-            this.lblLastName.Location = new System.Drawing.Point(15, 127);
+            this.lblLastName.Location = new System.Drawing.Point(15, 140);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.lblLastName.Size = new System.Drawing.Size(99, 24);
+            this.lblLastName.Size = new System.Drawing.Size(90, 23);
             this.lblLastName.TabIndex = 2;
             this.lblLastName.Text = "Last Name";
             // 
@@ -92,11 +91,11 @@
             // 
             this.lblAge.AutoSize = true;
             this.lblAge.BackColor = System.Drawing.Color.Transparent;
-            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAge.ForeColor = System.Drawing.Color.White;
-            this.lblAge.Location = new System.Drawing.Point(69, 173);
+            this.lblAge.Location = new System.Drawing.Point(69, 191);
             this.lblAge.Name = "lblAge";
-            this.lblAge.Size = new System.Drawing.Size(45, 24);
+            this.lblAge.Size = new System.Drawing.Size(39, 23);
             this.lblAge.TabIndex = 3;
             this.lblAge.Text = "Age";
             // 
@@ -104,11 +103,11 @@
             // 
             this.lblBirthday.AutoSize = true;
             this.lblBirthday.BackColor = System.Drawing.Color.Transparent;
-            this.lblBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthday.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblBirthday.ForeColor = System.Drawing.Color.White;
-            this.lblBirthday.Location = new System.Drawing.Point(37, 217);
+            this.lblBirthday.Location = new System.Drawing.Point(36, 254);
             this.lblBirthday.Name = "lblBirthday";
-            this.lblBirthday.Size = new System.Drawing.Size(77, 24);
+            this.lblBirthday.Size = new System.Drawing.Size(78, 23);
             this.lblBirthday.TabIndex = 4;
             this.lblBirthday.Text = "Birthday";
             // 
@@ -116,11 +115,11 @@
             // 
             this.lblProgram.AutoSize = true;
             this.lblProgram.BackColor = System.Drawing.Color.Transparent;
-            this.lblProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgram.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProgram.ForeColor = System.Drawing.Color.White;
-            this.lblProgram.Location = new System.Drawing.Point(308, 86);
+            this.lblProgram.Location = new System.Drawing.Point(308, 95);
             this.lblProgram.Name = "lblProgram";
-            this.lblProgram.Size = new System.Drawing.Size(82, 24);
+            this.lblProgram.Size = new System.Drawing.Size(79, 23);
             this.lblProgram.TabIndex = 5;
             this.lblProgram.Text = "Program";
             // 
@@ -128,11 +127,11 @@
             // 
             this.lblFirstName.AutoSize = true;
             this.lblFirstName.BackColor = System.Drawing.Color.Transparent;
-            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFirstName.ForeColor = System.Drawing.Color.White;
-            this.lblFirstName.Location = new System.Drawing.Point(308, 127);
+            this.lblFirstName.Location = new System.Drawing.Point(308, 140);
             this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(101, 24);
+            this.lblFirstName.Size = new System.Drawing.Size(103, 23);
             this.lblFirstName.TabIndex = 6;
             this.lblFirstName.Text = "First Name";
             // 
@@ -140,11 +139,11 @@
             // 
             this.lblMiddleInitial.AutoSize = true;
             this.lblMiddleInitial.BackColor = System.Drawing.Color.Transparent;
-            this.lblMiddleInitial.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleInitial.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMiddleInitial.ForeColor = System.Drawing.Color.White;
-            this.lblMiddleInitial.Location = new System.Drawing.Point(569, 127);
+            this.lblMiddleInitial.Location = new System.Drawing.Point(569, 140);
             this.lblMiddleInitial.Name = "lblMiddleInitial";
-            this.lblMiddleInitial.Size = new System.Drawing.Size(40, 24);
+            this.lblMiddleInitial.Size = new System.Drawing.Size(37, 23);
             this.lblMiddleInitial.TabIndex = 7;
             this.lblMiddleInitial.Text = "M.I.";
             // 
@@ -152,11 +151,11 @@
             // 
             this.lblGender.AutoSize = true;
             this.lblGender.BackColor = System.Drawing.Color.Transparent;
-            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblGender.ForeColor = System.Drawing.Color.White;
-            this.lblGender.Location = new System.Drawing.Point(308, 173);
+            this.lblGender.Location = new System.Drawing.Point(308, 191);
             this.lblGender.Name = "lblGender";
-            this.lblGender.Size = new System.Drawing.Size(74, 24);
+            this.lblGender.Size = new System.Drawing.Size(67, 23);
             this.lblGender.TabIndex = 8;
             this.lblGender.Text = "Gender";
             // 
@@ -164,21 +163,23 @@
             // 
             this.lblContactNo.AutoSize = true;
             this.lblContactNo.BackColor = System.Drawing.Color.Transparent;
-            this.lblContactNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContactNo.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblContactNo.ForeColor = System.Drawing.Color.White;
-            this.lblContactNo.Location = new System.Drawing.Point(391, 217);
+            this.lblContactNo.Location = new System.Drawing.Point(390, 254);
             this.lblContactNo.Name = "lblContactNo";
-            this.lblContactNo.Size = new System.Drawing.Size(108, 24);
+            this.lblContactNo.Size = new System.Drawing.Size(99, 23);
             this.lblContactNo.TabIndex = 9;
             this.lblContactNo.Text = "Contact No.";
             // 
             // btnRegister
             // 
-            this.btnRegister.BackColor = System.Drawing.Color.Thistle;
+            this.btnRegister.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnRegister.Location = new System.Drawing.Point(297, 276);
+            this.btnRegister.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRegister.Location = new System.Drawing.Point(278, 334);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.Size = new System.Drawing.Size(154, 50);
             this.btnRegister.TabIndex = 10;
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = false;
@@ -186,42 +187,42 @@
             // 
             // txtBoxStudentNo
             // 
-            this.txtBoxStudentNo.Location = new System.Drawing.Point(130, 86);
+            this.txtBoxStudentNo.Location = new System.Drawing.Point(130, 95);
             this.txtBoxStudentNo.Name = "txtBoxStudentNo";
             this.txtBoxStudentNo.Size = new System.Drawing.Size(159, 20);
             this.txtBoxStudentNo.TabIndex = 11;
             // 
             // txtBoxLastName
             // 
-            this.txtBoxLastName.Location = new System.Drawing.Point(130, 131);
+            this.txtBoxLastName.Location = new System.Drawing.Point(130, 144);
             this.txtBoxLastName.Name = "txtBoxLastName";
             this.txtBoxLastName.Size = new System.Drawing.Size(159, 20);
             this.txtBoxLastName.TabIndex = 12;
             // 
             // txtBoxFirstName
             // 
-            this.txtBoxFirstName.Location = new System.Drawing.Point(415, 131);
+            this.txtBoxFirstName.Location = new System.Drawing.Point(415, 144);
             this.txtBoxFirstName.Name = "txtBoxFirstName";
             this.txtBoxFirstName.Size = new System.Drawing.Size(148, 20);
             this.txtBoxFirstName.TabIndex = 13;
             // 
             // txtBoxMiddleInitial
             // 
-            this.txtBoxMiddleInitial.Location = new System.Drawing.Point(615, 131);
+            this.txtBoxMiddleInitial.Location = new System.Drawing.Point(615, 144);
             this.txtBoxMiddleInitial.Name = "txtBoxMiddleInitial";
             this.txtBoxMiddleInitial.Size = new System.Drawing.Size(65, 20);
             this.txtBoxMiddleInitial.TabIndex = 14;
             // 
             // txtBoxAge
             // 
-            this.txtBoxAge.Location = new System.Drawing.Point(130, 178);
+            this.txtBoxAge.Location = new System.Drawing.Point(130, 196);
             this.txtBoxAge.Name = "txtBoxAge";
             this.txtBoxAge.Size = new System.Drawing.Size(100, 20);
             this.txtBoxAge.TabIndex = 15;
             // 
             // txtBoxContactNo
             // 
-            this.txtBoxContactNo.Location = new System.Drawing.Point(509, 221);
+            this.txtBoxContactNo.Location = new System.Drawing.Point(508, 258);
             this.txtBoxContactNo.Name = "txtBoxContactNo";
             this.txtBoxContactNo.Size = new System.Drawing.Size(137, 20);
             this.txtBoxContactNo.TabIndex = 16;
@@ -229,7 +230,7 @@
             // cbBoxProgram
             // 
             this.cbBoxProgram.FormattingEnabled = true;
-            this.cbBoxProgram.Location = new System.Drawing.Point(415, 91);
+            this.cbBoxProgram.Location = new System.Drawing.Point(415, 100);
             this.cbBoxProgram.Name = "cbBoxProgram";
             this.cbBoxProgram.Size = new System.Drawing.Size(265, 21);
             this.cbBoxProgram.TabIndex = 17;
@@ -237,14 +238,14 @@
             // cbBoxGender
             // 
             this.cbBoxGender.FormattingEnabled = true;
-            this.cbBoxGender.Location = new System.Drawing.Point(415, 176);
+            this.cbBoxGender.Location = new System.Drawing.Point(415, 194);
             this.cbBoxGender.Name = "cbBoxGender";
             this.cbBoxGender.Size = new System.Drawing.Size(121, 21);
             this.cbBoxGender.TabIndex = 18;
             // 
             // dtBirthday
             // 
-            this.dtBirthday.Location = new System.Drawing.Point(130, 221);
+            this.dtBirthday.Location = new System.Drawing.Point(129, 258);
             this.dtBirthday.Name = "dtBirthday";
             this.dtBirthday.Size = new System.Drawing.Size(228, 20);
             this.dtBirthday.TabIndex = 19;
@@ -253,9 +254,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.Color.RoyalBlue;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(692, 327);
+            this.ClientSize = new System.Drawing.Size(749, 416);
             this.Controls.Add(this.dtBirthday);
             this.Controls.Add(this.cbBoxGender);
             this.Controls.Add(this.cbBoxProgram);
